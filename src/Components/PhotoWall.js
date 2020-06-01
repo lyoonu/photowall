64b77ac4264a1ab67e7730b2912460a5898d60fa
lyoonu <lyoonu17@gmail.com>
2020-06-01 17:44:56 +0900

    first commit

---
+++ src/Components/PhotoWall.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Photo from "./Photo";
+
+function PhotoWall(props) {
+  return (
+    <div className="photoGrid">
+      {props.posts.map((post, index) => (
+        <Photo post={post} key={index} />
+      ))}
+    </div>
+  );
+}
+
+export default PhotoWall;
