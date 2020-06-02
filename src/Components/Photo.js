2c343797cd478455486b00b277fca29da4ba0d08
lyoonu <lyoonu17@gmail.com>
2020-06-02 16:34:16 +0900

    Second commit before routing

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Photo(props) {
   const post = props.post;
@@ -9,10 +10,22 @@ function Photo(props) {
         <p>{post.description}</p>
       </figcaption>
       <div className="button-container">
-        <button className="remove-button">Remove</button>
+        <button
+          className="remove-button"
+          onClick={() => {
+            props.onRemovePhoto(post);
+          }}
+        >
+          Remove
+        </button>
       </div>
     </figure>
   );
 }
+
+Photo.propTypes = {
+  post: PropTypes.object.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired,
+};
 
 export default Photo;
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Photo from "./Photo";
+import PropTypes from "prop-types";
 
 function PhotoWall(props) {
   return (
     <div className="photoGrid">
       {props.posts.map((post, index) => (
-        <Photo post={post} key={index} />
+        <Photo post={post} key={index} onRemovePhoto={props.onRemovePhoto} />
       ))}
     </div>
   );
 }
+
+PhotoWall.propTypes = {
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired,
+};
 
 export default PhotoWall;
